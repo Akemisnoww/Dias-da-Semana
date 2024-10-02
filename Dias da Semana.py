738fef214ab5603d0ee8b17e3f931be7851ecb0e
Akemi 3 <akemisnoww@gmail.com>
2024-10-02 15:08:29 -0300

    Add files via upload
    Este programa mostra na tela os 7 dias da semana, Apenas troque o valor a cima, Se o dia da semana não for correspondente ao valor, ele mostra um texto dizendo "Outro Dia".

---
+++ Dias da Semana.py
@@ -0,0 +1,18 @@
+dia = 1
+
+if dia == 1:
+    print('domigo')
+elif dia == 2:
+    print('segunda-Feira')
+elif dia == 3:
+    print('terça-Feira')
+elif dia == 4:
+    print('quarta-feira')
+elif dia == 5:
+    print('quinta feira')
+elif dia == 6:
+    print('sexta feira')
+elif dia == 7:
+    print('sábado')
+else:
+    print('outro dia')
